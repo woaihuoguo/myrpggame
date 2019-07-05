6079018f3dc2cf7de2e6fd56b1c9258880a4cd07
woaihuoguo <49384650+woaihuoguo@users.noreply.github.com>
2019-07-05 20:57:27 +0800

    Update mygame.py

--- mygame.py
+++ mygame.py
@@ -15,9 +15,9 @@ class player:
 
 class Magician:
     def __init__ (self):
-        self.life=random.randint(85,100)
+        self.life=random.randint(80,90)
         self.skill=["FireBall"]*4+["IceArrow"]*4+["Meteorolite"]
-        self.attack=[5]*4+[3]*4+[20]
+        self.attack=[6]*3+[4]*3+[20]
         self.hit=100  
         self.trick=0
     def damaged(self,d):
@@ -37,22 +37,22 @@ class Warrior:
     def __init__ (self):
         self.life=random.randint(105,120)
         self.skill=["Pierce"]*2+["Wave"]*2+["Tranple"]
-        self.attack=[4]*2+[3]*2+[6]
+        self.attack=[4]*2+[4]*2+[7]
         self.hit=70  
         self.trick=0
     def damaged(self,d):
         self.life -= d
 
     def special(self,p1,p2):
-        m="{} has recovered 5 HP from the attack"
+        m="{} has recovered 7 HP from the attack"
         print(m.format(p1.name))
-        self.life+=5
+        self.life+=7
 
 class Bowman:
     def __init__ (self):
         self.life=random.randint(85,95)
         self.skill=["Shoot"]+["Discharge"]+["Snipe"]
-        self.attack=[3]+[3]+[5]
+        self.attack=[4]+[4]+[6]
         self.hit=85  
         self.trick=0
     def damaged(self,d):
@@ -79,40 +79,19 @@ class battle:
         if spn <=20 :
             p1.oc.special(p1,p2)
         
-            """
-        m="{} life:{}       {} life:{}"
-        print(m.format(p1.name,p1.oc.life,p2.name,p2.oc.life))
-        time.sleep(2)
-        """
-  
+
 class Game:
     def __init__ (self):
-        """
+        
         name1=input("player1 name:")
         oc1=input("player1 occupation(Magician/Warrior/Bowman):")
         name2=input("player2 name:")
         oc2=input("player2 occupation(Magician/Warrior/Bowman):")
-        """
-        name1="xhou"
-        oc1="B"
-        name2="zzh"
-        oc2="M"
         o1=self.chooseoc(oc1)
         o2=self.chooseoc(oc2)
-        p1=player(name1,o1)
-        p2=player(name2,o2)
-        while (p1.oc.life)*(p2.oc.life) > 0:
-            battle(p1,p2)
-            m="{} life:{}       {} life:{}\n"
-            print(m.format(p1.name,p1.oc.life,p2.name,p2.oc.life))
-            time.sleep(2)
-            if p2.oc.life <=0 :
-                self.wins(p1.name)
-            battle(p2,p1)
-            m="{} life:{}       {} life:{}\n"
-            print(m.format(p1.name,p1.oc.life,p2.name,p2.oc.life))
-            time.sleep(2)            
-        self.wins(p2.name)
+        self.p1=player(name1,o1)
+        self.p2=player(name2,o2)
+        
     def wins(self,winner):
         m="{} has won!"
         print(m.format(winner))
@@ -125,62 +104,20 @@ class Game:
             return Warrior()
         if oc is "B":
             return Bowman()
-        """
-    def begin(self):
-        
-        while (p1.oc.life)*(p2.oc.life) > 0:
-            battle(p1,p2)
+       
+    def begin(self):    
+        while (self.p1.oc.life)*(self.p2.oc.life) > 0:
+            battle(self.p1,self.p2)
             m="{} life:{}       {} life:{}\n"
-            print(m.format(p1.name,p1.oc.life,p2.name,p2.oc.life))
+            print(m.format(self.p1.name,self.p1.oc.life,self.p2.name,self.p2.oc.life))
             time.sleep(2)
-            if p2.oc.life <=0 :
-                Game.wins(p1.name)
-            battle(p2,p1)
+            if self.p2.oc.life <=0 :
+                self.wins(self.p1.name)
+                return
+            battle(self.p2,self.p1)
             m="{} life:{}       {} life:{}\n"
-            print(m.format(p1.name,p1.oc.life,p2.name,p2.oc.life))
+            print(m.format(self.p1.name,self.p1.oc.life,self.p2.name,self.p2.oc.life))
             time.sleep(2)            
-        Game.wins(p2.name)
-      """
-""" 
-def win(winner):
-    m="{} has won!"
-    print(m.format(winner))
-        
-
-
-def chooseoc(oc):
-    if oc is "M":
-        return Magician()
-    if oc is "W":
-        return Warrior()
-    if oc is "B":
-        return Bowman()   
- 
-name1=input("player1 name:")
-oc1=input("player1 occupation(Magician/Warrior/Bowman):")
-name1=input("player2 name:")
-oc2=input("player2 occupation(Magician/Warrior/Bowman):")
-"""
-"""
-name1="xhou"
-oc1="B"
-name2="zzh"
-oc2="M"
-o1=chooseoc(oc1)
-o2=chooseoc(oc2)
-p1=player(name1,o1)
-p2=player(name2,o2)         
-while (p1.oc.life)*(p2.oc.life) > 0:
-    battle(p1,p2)
-    m="{} life:{}       {} life:{}\n\n"
-    print(m.format(p1.name,p1.oc.life,p2.name,p2.oc.life))
-    time.sleep(2)
-    if p2.oc.life <=0 :
-        win(p1.name)
-        
-    battle(p2,p1)
-    m="{} life:{}       {} life:{}\n\n"
-    print(m.format(p1.name,p1.oc.life,p2.name,p2.oc.life))
-    time.sleep(2)
-win(p2.name)
-"""
+            if self.p1.oc.life <=0 :
+                self.wins(self.p2.name)
+                return
